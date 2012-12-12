7bc8dfd9269df87e3fb2eb4f87898dd886c097b8
Daniel P. Clark <webmaster@6ftdan.com>
2012-12-11 22:39:01 -0500

    Fixed incompatibility with Ruby > 1.8.7 in String.match_pci

--- lib/mightystring/string_matchpci.rb
+++ lib/mightystring/string_matchpci.rb
@@ -2,13 +2,17 @@
 # by Daniel P. Clark
 # webmaster@6ftdan.com
 
+# BUMP > after find_all was depreciated from Ruby 1.8.7 this didn't work.
+# And it turns out this is a lot simpler then expected.
+# Here for backwards compatibility.
+
 # Match Partial Case-Insensitive
 module Match_PCI
     module String
     	# Match Partial Case-Insensitive: Usage: "My string has this?".matchpci('RinG') => true
         def match_pci(in_srch = "")
         	if not in_srch.empty?
-			return !self.find_all{|item| item.downcase.include?(in_srch.downcase)}.empty?
+			return !!self.downcase[in_srch.downcase]
 		end
 		return false
 	end
